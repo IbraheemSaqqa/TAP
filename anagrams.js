c539816223fddb46c9aeefddc3dc2cdc3983580b
IbraheemSaqqa <83614820+IbraheemSaqqa@users.noreply.github.com>
2023-08-02 14:20:51 +0300

    Add files via upload

---
+++ anagrams.js
@@ -0,0 +1,39 @@
+/*
+Example input: treat, ["tater", "tree", "teart", "tetra", "heart", "hamster"]
+Example output: ["tater", "teart", "tetra"]
+*/
+
+const anagrams = [
+    "oz bilal tochberer",  
+    "it's razorbill beachcomber", 
+    "och robe tilblazer", 
+    "bib chorizo cellarmaster", 
+    "thor bble carizole", 
+    "zll borcht aberoie", 
+    "brzl orche atobile", 
+    "dame shelburne characterizing", 
+    "uber englishman characterized", 
+    "agnes rhumbline characterized", 
+    "rehab scrutinized charlemagne", 
+    "dreams zurich interchangeable", 
+    "bam hamster technocratic", 
+    "mechatronic masterbatch", 
+    "bam ratchet mechatronics"
+    ]
+
+      function isAnagramInArray(anagram, arr) {
+        const sortedAnagram = anagram.toLowerCase().split("").sort().join("");
+        const arrayOfAnagrams = [];
+      
+        arr.forEach((item) => {
+          const sortedItem = item.toLowerCase().split("").sort().join("");
+          if (sortedItem === sortedAnagram) {
+            arrayOfAnagrams.push(item);
+          }
+        });
+      
+        return arrayOfAnagrams;
+      }
+      
+
+console.log(isAnagramInArray("Bob Ziroll Teacher", anagrams));
